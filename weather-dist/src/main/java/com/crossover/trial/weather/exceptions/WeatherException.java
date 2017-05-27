2bd88c323a06bd9321c3ed9e7291fd41745c754c
George Silva <georgehg@hotmail.com>
2017-05-27 18:48:08 -0300

    First commit

---
+++ weather-dist/src/main/java/com/crossover/trial/weather/exceptions/WeatherException.java
@@ -0,0 +1,7 @@
+package com.crossover.trial.weather.exceptions;
+
+/**
+ * An internal exception marker
+ */
+public class WeatherException extends Exception {
+}
---
+++ weather-dist/src/main/java/com/crossover/trial/weather/model/DataPoint.java
@@ -0,0 +1,101 @@
+package com.crossover.trial.weather.model;
+
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
+
+/**
+ * A collected point, including some information about the range of collected values
+ *
+ * @author code test administrator
+ */
+@ToString
+@EqualsAndHashCode
+public class DataPoint {
+
+	private double mean = 0.0;
+	
+	private int first = 0;
+
+	private int second = 0;
+
+	private int third = 0;
+
+	private int count = 0;
+	
+    /** private constructor, use the builder to create this object */
+    private DataPoint(double mean, int first, int second, int third, int count) {
+    	this.mean = mean;
+    	this.first = first;
+        this.second = second;
+        this.third = third;
+        this.count = count;
+    }
+
+    /** the mean of the observations */
+    public double getMean() {
+        return mean;
+    }
+
+    /** 1st quartile -- useful as a lower bound */
+    public int getFirst() {
+        return first;
+    }
+
+    /** 2nd quartile -- median value */
+    public int getSecond() {
+        return second;
+    }
+
+    /** 3rd quartile value -- less noisy upper value */
+    public int getThird() {
+        return third;
+    }
+
+    /** the total number of measurements */
+    public int getCount() {
+        return count;
+    }
+    
+    public static DataPoint.Builder builder() {
+    	return new DataPoint.Builder();
+    }
+
+    public static final class Builder {
+    	private double mean;
+        private int first;
+        private int second;
+        private int third;
+        private int count;
+
+        public Builder() {}
+        
+        public Builder withMean(double mean) {
+        	this.mean = mean;
+        	return this;
+        }
+
+        public Builder withFirst(int first) {
+            this.first= first;
+            return this;
+        }
+
+        public Builder withSecond(int second) {
+        	this.second = second;
+            return this;
+        }
+
+        public Builder withThird(int third) {
+        	this.third = third;
+        	return this;
+        }
+        
+        public Builder withCount(int count) {
+        	this.count = count;
+            return this;
+        }
+
+        public DataPoint build() {
+            return new DataPoint(this.mean, this.first, this.second, this.third, this.count);
+        }
+    }
+}
